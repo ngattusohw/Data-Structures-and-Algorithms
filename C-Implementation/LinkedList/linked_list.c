c17f68133baf699b7d5bd94ac187c13b4e07631b
ngattusohw <ngattusohw@gmail.com>
2017-02-02 22:21:57 -0500

    Adding reversing list function

--- C-Implementation/LinkedList/linked_list.c
+++ C-Implementation/LinkedList/linked_list.c
@@ -9,10 +9,21 @@
 
 
 void reverse_list(List* the_list){
+	Node currentNode = the_list->head->next;
+	head->next=null;
+	Node hold_next=currentNode->next;
+	Node hold_prev = the_list->head;
 
+	while(hold_prev!=tail){
+		hold_next = currentNode->next;
+		currentNode->next = hold_prev;
+		hold_prev = currentNode;
+		currentNode = hold;
+	}
 
-
-
+	hold_next = the_list->head;
+	the_list->head = the_list->tail;
+	the_list->tail = hold_next;
 
 
 }
