f6b93ec50f9c8096e674ab849a35c840d96a95d4
ngattusohw <ngattusohw@gmail.com>
2017-08-07 22:34:57 -0400

    Adding known functions needed for hashtable to .h file

--- C++-Implementation/HashTable/hashtable.h
+++ C++-Implementation/HashTable/hashtable.h
@@ -1,7 +1,22 @@
 #ifndef HASH_TABLE
 #define HASH_TABLE
 
-
+class hashtable
+{
+public:
+	hashtable();
+	~hashtable();
+	int getCapacity();
+	int getSize();
+	void insert(int elem);
+	int hash(int elem);
+private:
+	LinkedList list;
+	int* buckets;
+	int size;
+	int cap;
+	double load;
+};
 
 
 
